49bf283f03f1daabdd138418de0637034a6c5cbc
Christian Labonté <cbonte99@gmail.com>
2020-08-04 18:26:19 -0400

    Completed initial data model for sample app

--- src/Twig/MySqlTwigExtension.php
+++ src/Twig/MySqlTwigExtension.php
@@ -170,7 +170,7 @@ class MySqlTwigExtension extends SqlTwigExtension
     public function columnFilter($obj): string
     {
         if ($obj instanceof Index) {
-            // MySQL has some limits on the size of indexes for varchar columns, let's handle it propelrly
+            // MySQL has some limits on the size of indexes for varchar columns, let's handle it properly
             $columns = [];
             foreach ($obj->getFields() as $field) {
                 $size = null;
