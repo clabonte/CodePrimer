a1930ef1b54c9a44d3b8acfe86f988cc169896c0
Christian Labonté <cbonte99@gmail.com>
2020-09-09 12:00:59 -0400

    Fixed broken test

--- fixtures/configuration/codeprimer/bundle.php
+++ fixtures/configuration/codeprimer/bundle.php
@@ -2,7 +2,7 @@
 
 use CodePrimer\Model\BusinessBundle;
 
-if (!defined('prepareBundle')) {
+if (!function_exists('prepareBundle')) {
     require 'DatasetFactory.php';
     require 'BusinessModelFactory.php';
     require 'BusinessProcessFactory.php';
